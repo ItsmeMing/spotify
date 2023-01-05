b564af3a381fc442cf7a184a9bfa655b99de620f
ItsmeMing <ngocminh.vu.520@gmail.com>
2023-01-05 16:58:48 +0700

    updated

--- src/repositories/Clients/AxiosClient.js
+++ src/repositories/Clients/AxiosClient.js
@@ -3,11 +3,17 @@ import axios from "axios";
 const baseDomain = "https://api.spotify.com/v1/";
 const baseURL = `${baseDomain}`;
 
+const access_token = () => {
+    if ((Date.now() - localStorage.getItem("access_token")) / 1000 < 3600)
+        return localStorage.getItem("access_token");
+    else return new URLSearchParams(window.location.search).get("access_token");
+};
+
 export default axios.create({
     baseURL,
     headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-        "Accept": "application/json",
-        "Content-Type": "application/json"
+        Authorization: `Bearer ${access_token()}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
     },
 });
